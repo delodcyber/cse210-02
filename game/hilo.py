4959070d200aba6b98241a063831790e6f0fe818
TeddyTjoe1990 <her21059@byui.edu>
2022-01-26 15:36:54 +0700

    Initial Commit

--- game/hilo.py
+++ game/hilo.py
@@ -1,3 +1,4 @@
+print(">Welcome to HiLo Games<")
 import random
 
 class Hilo:
@@ -7,8 +8,34 @@ class Hilo:
     The next card will be displayed. 
     """
 
-    def __init__(self):
-        self.point = 300
+    def __init__(self) -> None:
+        """
+        Initialize two random cards.
+        """
+        self.card1 = random.randint(1,14)
+        self.card2 = random.randint(1,14)
 
-    def shuffle(self):
-        pass
+    def show_card1(self):
+        """
+        Display first card to the user.
+        """
+        output = f'The card is: {self.card1}'
+        print(output)
+
+    def show_card2(self):
+        """
+        Display second card to the user. 
+        """
+        output = f'The card is: {self.card2}'
+        print(output)
+
+
+    def high_or_low(self):
+        """
+        Check high card or low card. 
+        """
+        if self.card2 > self.card1:     # check for high card
+            return 1
+        elif self.card2 < self.card1:   # check for low card
+            return -1 
+        
--- game/player.py
+++ game/player.py
@@ -1,34 +1,77 @@
-import hilo
+import random
+from hilo import Hilo
 
 class Player:
-    """
-    The player direct control the game. 
-    Player starts the game with 300 points.
-    If player guess higher or lower:
-    - The player earns 100 points if guess is correct.
-    - The player loses 75 points if they guessed is incorrectly. 
-    - If player reaches 0 points the game is over. 
-    Player decides to keep playing or stop the game. 
-    """
     def __init__(self) -> None:
         """ 
         Construct a player
+        Construct a player object.
         """
+        c = Hilo()
         self.points = 300
-        self.guess = ""
         self.is_playing = True
         self.total_points = 0
-
+        self.card1 = random.randint(1,14)
+        self.card2 = random.randint(1,14)
+        
     def play_game(self):
         """
         Start the game Hilo 
         """
         while self.is_playing:
-            pass
+            card = Hilo()
+            car1 = card.show_card1()
+            self.guess_hilo()
+            self.update_points()
+            car2 = card.show_card2()
+            self.display_score()
+            self.play_again()
+
     def guess_hilo(self):
         """
-        Ask user for a higher or lower guess. 
+        #Ask user for a higher or lower guess. 
         """
         guess = input("Higher or lower? [h/l] ")
+        return guess
 
-        
+    def update_points(self):
+        """
+        It will add or substract point from the user. 
+        """
+        if (Hilo.high_or_low == 1 and self.guess_hilo == "h") or (Hilo.high_or_low == -1 and self.guess_hilo == "l"):
+            self.points += 100
+        elif (Hilo.high_or_low == 1 and self.guess_hilo == "l") or (Hilo.high_or_low == -1 and self.guess_hilo == "h"):
+            self.points -= 75
+                # if (self.guess_hilo == 1 and self.card2 > self.card1):
+                #     self.points += 100
+                # elif (self.guess_hilo == -1 and self.card2 < self.card1):
+                #     self.points -= 75
+                # elif (self.guess_hilo == 1 and self.card2 < self.card1):
+                #     self.points += 100
+                # elif (self.guess_hilo == -1 and self.card2 > self.card1):
+                #     self.points -= 75
+                #else:
+                #    print("Draw!")
+
+    def display_score(self):
+        """
+        Display final score to the player.
+        """
+        self.total_points = self.points 
+        print(f'Your score is:  {self.total_points}')
+
+    def play_again(self):
+        """
+        Ask the user if they want to play again and check if points are not 0
+        """
+        again = input("Play again? [y/n] ")
+        if self.points > 0 and again == "y":
+            self.is_playing = True
+        elif self.points <= 0 or again == "n":
+            print("Game Over")
+            print(f"Total score: {self.total_points}")
+            self.is_playing = False
+        print()
+
+p = Player()
+p.play_game()
