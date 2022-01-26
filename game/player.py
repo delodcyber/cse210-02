4812b487cb30950fe56cb1ee1531be68c69f3be1
juliwhite <julietawhite30@gmail.com>
2022-01-25 20:29:01 -0700

    updated files

--- game/player.py
+++ game/player.py
@@ -1,4 +1,4 @@
-import hilo
+from hilo import Hilo
 
 class Player:
     """
@@ -12,23 +12,64 @@ class Player:
     """
     def __init__(self) -> None:
         """ 
-        Construct a player
+        Construct a player object.
         """
         self.points = 300
-        self.guess = ""
+        #self.guess = ""
         self.is_playing = True
         self.total_points = 0
+        c = Hilo()
 
     def play_game(self):
         """
         Start the game Hilo 
         """
         while self.is_playing:
-            pass
+            Hilo.show_card1(self)
+            self.guess_hilo()
+            self.update_points()
+            Hilo.show_card2(self)
+            self.display_score()
+            self.play_again()
+        
+
     def guess_hilo(self):
-        """
+        """ 
         Ask user for a higher or lower guess. 
         """
         guess = input("Higher or lower? [h/l] ")
+        return guess
+
+    def update_points(self):
+        """
+        It will add or substract point from the user. 
+        """
+        if (Hilo.high_or_low == 1 and self.guess_hilo == "h") or (Hilo.high_or_low == -1 and self.guess_hilo == "l"):
+            self.points += 100
+        elif (Hilo.high_or_low == 1 and self.guess_hilo == "l") or (Hilo.high_or_low == -1 and self.guess_hilo == "h"):
+            self.points -= 75
+    
+    def display_score(self):
+        """
+        Display final score to the player.
+        """
+        self.total_points = self.points
+        print(f'Your score is:  {self.total_points}')
+
+    def play_again(self):
+        """
+        Ask the user if they want to play again and check if points are not 0
+        """
+        again = input("Play again? [y/n] ")
+        if self.points > 0 and again == "y":
+            self.is_playing = True
+        elif self.points <= 0 or again == "n":
+            print("Game is over.")
+            self.is_playing = False
+
+p = Player()
+p.play_game()
+
+
 
         
